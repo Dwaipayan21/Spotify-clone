e6d7bc2aacf3aafcfdc3e102294289b9076789f5
dwaipayan <dwaipayanbarui99209@gmail.com>
2026-07-28 12:19:20 +0530

    Implement authentication flow with Clerk, add Axios for API calls, and enhance UI components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignIn, SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 
 function App() {
@@ -12,6 +11,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element= {<HomePage />} />
+        <Route path="/sso-callback" element= {<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />} />
         <Route path="/auth-callback" element= {<AuthCallbackPage />} />
       </Routes>
     </>
