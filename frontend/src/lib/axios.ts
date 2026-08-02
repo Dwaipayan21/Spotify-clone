1e8f6b736a50885f79eba7da7690c617e6036d7b
dwaipayan <dwaipayanbarui99209@gmail.com>
2026-08-03 02:30:44 +0530

    project completed

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -7,7 +7,7 @@ declare global {
 }
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE= "development" ? "http://localhost:5000/api" : "/api",
+    baseURL: import.meta.env.MODE ===  "development" ? "http://localhost:5000/api" : "/api",
 });
 
 axiosInstance.interceptors.request.use(async (config) => {
